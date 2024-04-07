584808a7375327ada1fd0881e933ac8cb9ce7681
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-08 00:01:25 +0200

    Bejelentkezési token letárolása

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,53 @@
+import { useRef } from "react";
+
+
 function LoginForm() {
-    return (  <form style={{marginTop: "5px", marginBottom: "5px"}}>
+    const apiUrl ="http://localhost:8000/api";
+    const emailRef = useRef(null);
+    const jelszoRef = useRef(null);
+   
+   
+    const handleformSubmit = async (event) =>{
+        event.preventDefault();
+
+        const user = {
+            email:emailRef.current.value,
+            jelszo:jelszoRef.current.value,
+    };
+    login(user)
+}
+//regisztracios függvény ami felhasználói adatokat vár eredményül
+const login = async userData => {
+    const url = apiUrl + "/login";
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+    const data = await response.json();
+    console.log(data);
+    if  (response.ok) {
+        alert("Sikeres bejelentkezés")
+    }else {
+        alert(data.message);
+    }
+
+}
+
+    return (  <form style={{marginTop: "5px", marginBottom: "5px"}} onSubmit={handleformSubmit}>
         <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="email" />
+            <label htmlFor="loginemail">Email</label>
+            <input type="email" id="loginemail" placeholder="email" ref={emailRef} />
         </div>
         <div>
-            <label htmlFor="jelszo">Jelszó</label>
-            <input type="password" id="jelszo" placeholder="jelszo" />
+            <label htmlFor="loginjelszo">Jelszó</label>
+            <input type="password" id="loginjelszo" placeholder="jelszo" ref={jelszoRef}/>
         </div>
         <button type="submit">Bejelentkezés</button>
-    </form>
-);
+    </form>);
 }
 
 export default LoginForm;
