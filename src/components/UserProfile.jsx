24d811c5a39cac733b02d420fb0e241c51d7731f
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-08 00:44:14 +0200

    userprofile es kijelentkezes

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,5 +1,18 @@
-function UserProfile() {
-    return ( <></> );
+import PropTypes from "prop-types";
+
+
+function UserProfile(props) {
+    const { user } = props;
+    return ( <div>
+        <p>Bejelentkezve: {user.name}</p>
+        <button type="button">Kijelentkezés</button>
+        <button type="button">Kijelentkezés mindenhonnan</button>
+    </div> );
 }
+
+UserProfile.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
 
 export default UserProfile;
