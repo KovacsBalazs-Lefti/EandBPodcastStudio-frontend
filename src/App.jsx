f59285f51cab7bfeb9b655ca4fccaabd984b8440
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-08 01:40:18 +0200

    token eltárolása a localstorage-ban

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,103 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import UserProfile from './components/UserProfile';
 
 function App () {
-  const loggedIn = false;
+  const apiUrl ="http://localhost:8000/api";
+  //token letárolása
+  const [token, setToken] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const loadUserData = async () => {
+    const url = apiUrl + "/user"
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer "+token
+      }
+    });
+    const data = await response.json();
+    if (response.ok){
+      setUserData(data);
+    }else{
+      setToken('');
+    }
+  }
+  //lekérdezés amikor a token megváltozik (uef)
+  useEffect(() => {
+    if (token){
+      loadUserData();
+    }else{
+      setUserData(null);
+    }
+  }, [token]);
+ 
+const login = async FormData => {
+  const url = apiUrl + "/login";
+  const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(FormData),
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      }
+  });
+  const data = await response.json();
+  console.log(data);
+  if  (response.ok) {
+      setToken(data.token);
+      alert("Sikeres bejelentkezés")
+  }else {
+      alert(data.message);
+  }
+
+}
+const logout = async () =>{
+  const url = apiUrl + "/logout";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + token
+    }
+  })
+  if (response.ok){
+    setToken('');
+  }else{
+    const data = await response.json();
+    alert(data.message);
+  }
+}
+
+const logoutEverywhere = async () => {
+  const url = apiUrl + "/logout-logoutEverywhere";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + token
+    }
+  })
+  if (response.ok){
+    setToken('');
+  }else{
+    const data = await response.json();
+    alert(data.message);
+  }
+}
+
   return (<main>
 
-    {loggedIn ?
-      <UserProfile />
+    {userData != null ?
+      <UserProfile user={userData} logoutClick={logout} logoutEverywhereClick={logoutEverywhere} />
       :
       <>
       <RegisterForm/>
-      <LoginForm/>
+      <LoginForm onSubmit={login}/>
       </>
     }
   </main>  );
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,16 +2,18 @@ import PropTypes from "prop-types";
 
 
 function UserProfile(props) {
-    const { user } = props;
+    const { user, logoutClick, logoutEverywhereClick } = props;
     return ( <div>
-        <p>Bejelentkezve: {user.name}</p>
-        <button type="button">Kijelentkezés</button>
-        <button type="button">Kijelentkezés mindenhonnan</button>
+        <p>Bejelentkezve: {user.nev}</p>
+        <button type="button" onClick={() => logoutClick()}>Kijelentkezés</button>
+        <button type="button"onClick={() => logoutEverywhereClick()}>Kijelentkezés mindenhonnan</button>
     </div> );
 }
 
 UserProfile.propTypes = {
-    user: PropTypes.object.isRequired
+    user: PropTypes.object.isRequired,
+    logoutClick: PropTypes.func.isRequired,
+    logoutEverywhereClick: PropTypes.func.isRequired
 }
 
 
