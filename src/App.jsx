289c4ab04ee439243268f8ba6ea9ede2413be9d0
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-08 15:19:52 +0200

    nav létrehozas

--- src/App.jsx
+++ src/App.jsx
@@ -5,135 +5,146 @@ import LoginForm from './pages/LoginForm';
 import RegisterForm from './pages/RegisterForm';
 import UserProfile from './pages/UserProfile';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
 
-function App () {
-  const apiUrl ="http://localhost:8000/api";
+function App() {
+  const apiUrl = "http://localhost:8000/api";
   //token letárolása
   const [token, setToken] = useState('');
   const [userData, setUserData] = useState(null);
 
-  const router = createBrowserRouter([{
-  path: "/",
-  element: <div>Hello World</div>
-  },
-  {
-    path: "/rolunk",
-    element: <div>Rólunk</div>
-  }
-
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <div>Rólunk</div>,
+        },
+        {
+          path: "/foglalas",
+          element: <div>Foglalás</div>,
+        },
+        {
+          path: "/regisztracio",
+          element: <div>Regisztráció</div>
+        },
+      ],
+    },
+  ]);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
-    if (storedToken){
+    if (storedToken) {
       setToken(storedToken);
     }
   }, []);
   //lekérdezés amikor a token megváltozik (uef)
   useEffect(() => {
-    if (token){
+    if (token) {
       loadUserData();
-    }else{
+    } else {
       setUserData(null);
     }
   }, [token]);
- 
 
 
-const login = async FormData => {
-  const url = apiUrl + "/login";
-  const response = await fetch(url, {
+
+  const login = async FormData => {
+    const url = apiUrl + "/login";
+    const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(FormData),
       headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
+        "Accept": "application/json",
+        "Content-Type": "application/json"
       }
-  });
-  const data = await response.json();
-  console.log(data);
-  if  (response.ok) {
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
       localStorage.setItem("token", data.token);
       setToken(data.token);
       alert("Sikeres bejelentkezés")
-  }else {
+    } else {
       alert(data.message);
-  }
-
-};
-
-const loadUserData = async () => {
-  const url = apiUrl + "/user"
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Accept": "application/json",
-      "Authorization": "Bearer "+token
     }
-  });
-  const data = await response.json();
-  if (response.ok){
-    setUserData(data);
-  }else{
-    localStorage.removeItem("token");
-    setToken('');
-  }
-};
 
+  };
 
-const logout = async () =>{
-  const url = apiUrl + "/logout";
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Bearer " + token
-    }
-  })
-  if (response.ok){
-    localStorage.removeItem("token");
-    setToken('');
-  }else{
+  const loadUserData = async () => {
+    const url = apiUrl + "/user"
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer " + token
+      }
+    });
     const data = await response.json();
-    alert(data.message);
-  }
-}
-
-const logoutEverywhere = async () => {
-  const url = apiUrl + "/logout-logoutEverywhere";
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Bearer " + token
+    if (response.ok) {
+      setUserData(data);
+    } else {
+      localStorage.removeItem("token");
+      setToken('');
     }
-  })
-  if (response.ok){
-    localStorage.removeItem("token");
-    setToken('');
-  }else{
-    const data = await response.json();
-    alert(data.message);
+  };
+
+
+  const logout = async () => {
+    const url = apiUrl + "/logout";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token
+      }
+    })
+    if (response.ok) {
+      localStorage.removeItem("token");
+      setToken('');
+    } else {
+      const data = await response.json();
+      alert(data.message);
+    }
   }
-}
+
+  const logoutEverywhere = async () => {
+    const url = apiUrl + "/logout-logoutEverywhere";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token
+      }
+    })
+    if (response.ok) {
+      localStorage.removeItem("token");
+      setToken('');
+    } else {
+      const data = await response.json();
+      alert(data.message);
+    }
+  }
 
   return (
-    
-  <div>
-    <RouterProvider router={router}/>
-    <main>
-      {userData != null ? (
-        <UserProfile user={userData} logoutClick={logout} logoutEverywhereClick={logoutEverywhere} />
-      )  : (
-        <>
-        <RegisterForm/>
-        <LoginForm onSubmit={login}/>
-        </>
-      )}
-    </main>
-  </div>  
+
+    <div>
+      <RouterProvider router={router} />
+      <main>
+        {userData != null ? (
+          <UserProfile user={userData} logoutClick={logout} logoutEverywhereClick={logoutEverywhere} />
+        ) : (
+          <>
+            <RegisterForm />
+            <LoginForm onSubmit={login} />
+          </>
+        )}
+      </main>
+    </div>
 
   );
 }
 
-export default App ;
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (  <nav>
+        <ul>
+            <li><a href="/">Rólunk</a></li>
+            <li><a href="/foglalas">Foglalás</a></li>
+            <li><a href="/regisztracio">Regisztráció</a></li>
+        </ul>
+    </nav>);
+}
+
+export default Navbar;
