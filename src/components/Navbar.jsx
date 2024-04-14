937132370b9b2b41632bc6328678534518e1b1f8
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-14 17:56:59 +0200

    navigáció formázása - kész

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import {Link} from "react-router-dom";
 function Navbar() {
     return (  
-    <nav className="navbar navbar-expand-lg bg-dark">
+    <nav className="navbar navbar-expand-lg data-bs-theme"data-bs-theme="dark">
         <div className="container-fluid">
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-            <div className="collapse navbar-collapse">
-                <ul>
-                    <li>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li className="nav-item">
                         <Link to="/">Rólunk</Link>
                     </li>
-                
-                    <li>
-                        <Link to="/register">Regisztráció</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Regisztráció</Link>
                     </li>
-                    <li>
-                        <Link to="/login">Bejelentkezés</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Bejelentkezés</Link>
                     </li>
-                    <li>
-                        <Link to="/user-profile">Profil</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/user-profile">Profil</Link>
                     </li>
                 </ul>
             </div>
