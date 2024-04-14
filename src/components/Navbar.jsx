3085dd42d3c16a6c32986f19549c065503b51389
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-14 18:56:03 +0200

    navbar javitasok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,34 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 function Navbar() {
-    return (  
-    <nav className="navbar navbar-expand-lg data-bs-theme"data-bs-theme="dark">
-        <div className="container-fluid">
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <div className="container-fluid">
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb2 mb-lg-0">
 
-                    <li className="nav-item">
-                        <Link to="/">Rólunk</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Regisztráció</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Bejelentkezés</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/user-profile">Profil</Link>
-                    </li>
-                </ul>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Rólunk</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">Regisztráció</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Bejelentkezés</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user-profile">Profil</Link>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>);
+        </nav>);
 }
 
 export default Navbar;
