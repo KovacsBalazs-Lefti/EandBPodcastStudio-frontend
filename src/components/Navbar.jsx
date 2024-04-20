da6d4a412644c41577e2d7fd139f61138f3f4476
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-20 17:55:52 +0200

    furcsa hibám van-a böngészőkben másképp megjelenések

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,47 +6,52 @@ function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
             <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb2 mb-lg-0">
+                        {leftSide.map((navItem) => (<li key={navItem.to} className="nav-item">
+                            <Link className="nav-link" to={navItem.to}>
+                                {navItem.text}
+                            </Link>
+                        </li>))}
 
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb2 mb-lg-0">
-                            {leftSide.map(navItem => <li key={navItem.to} className="nav-item">
-                                <Link className="nav-link" to={navItem.to}>
-                                    {navItem.text}
-                                </Link>
-                            </li>)}
-
-                        </ul>
-                        <ul className="navbar-nav">
-                            {rightSide.map(navItem => <li key={navItem.to} className="nav-item">
-                                <Link className="nav-link" to={navItem.to}>
-                                    {navItem.text}
-                                </Link>
-                            </li>)}
+                    </ul>
+                    <ul className="navbar-nav">
+                        {rightSide.map((navItem) => (<li key={navItem.to} className="nav-item">
+                            <Link className="nav-link" to={navItem.to}>
+                                {navItem.text}
+                            </Link>
+                        </li>))}
                         {
                             rightSideOthers.map(item => (
                                 <li
-                                    key = {item} className="nav-item">
+                                    key={item} className="nav-item">
                                     {item}
                                 </li>
                             ))
                         }
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
             </div>
-        </nav>)
+        </nav>
+    );
 }
 Navbar.propTypes = {
     leftSide: PropTypes.array.isRequired,
     rightSide: PropTypes.array.isRequired,
     rightSideOthers: PropTypes.array,
 }
-Navbar.DefaultProps ={
-    rightSideOthers:[]
+Navbar.DefaultProps = {
+    rightSideOthers: []
 }
 
 export default Navbar;
