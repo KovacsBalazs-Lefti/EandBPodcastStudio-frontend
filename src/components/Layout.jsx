f49ab111a66966bce790d7853b4263c3febca60f
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-23 23:58:32 +0200

    logo javítás mobilnézetben

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,48 +1,49 @@
 import Navbar from "./Navbar";
 import { useContext } from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 
 function Layout() {
     const authContext = useContext(AuthContext);
     //bejelentkezés kijelentkezés navigacio megvalósítása
-    const {authToken, logout} = authContext;
+    const { authToken, logout } = authContext;
     const navbarLeftSide = [];
     const navbarRightSide = [];
     const navbarRightSideOthers = [];
 
-    navbarLeftSide.push({ImgLogo: <img src="/images/logoblack.png" alt="Logo" style={{ height: '150px', width: 'auto', maxWidth: '100%' }} />, to: "/"});
-    navbarLeftSide.push({to: "/", text: "Rólunk"});
+    navbarLeftSide.push({ ImgLogo: <img src="/images/logoblack.png" alt="Logo"  style={{ height: '150px', width: 'auto', maxWidth: '100%' }} />,
+     to: "/", key:"Logo" });
+    navbarLeftSide.push({ to: "/", key: "Aboutus", text: "Rólunk" });
 
     if (authToken) {
-        navbarLeftSide.push({to: "/my-bookings", text: "Foglalásaim"});
-        navbarLeftSide.push({to: "/create-bookings", text: "Új Foglalás felvétele"});
-        navbarRightSide.push({to: "/user-profile", text: "Profil"});
+        navbarLeftSide.push({ to: "/my-bookings", text: "Foglalásaim" });
+        navbarLeftSide.push({ to: "/create-bookings", text: "Új Foglalás felvétele" });
+        navbarRightSide.push({ to: "/user-profile", text: "Profil" });
         navbarRightSideOthers.push(
-            <button className="nav-link" onClick={()=> logout()}>Kijelentkezés</button>
+            <button className="nav-link" onClick={() => logout()}>Kijelentkezés</button>
         );
-        
-        
-    } else{
-        navbarRightSide.push({to: "/login", text: "Bejelentkezés"});
-        navbarRightSide.push({to: "/register", text: "Regisztráció"});
+
+
+    } else {
+        navbarRightSide.push({ to: "/login", text: "Bejelentkezés" });
+        navbarRightSide.push({ to: "/register", text: "Regisztráció" });
     }
 
-    return ( 
-        
+    return (
+
         <main className="container-fluid main">
-        <div className="Header-img">
-        <Navbar leftSide={navbarLeftSide} rightSide={navbarRightSide} rightSideOthers={navbarRightSideOthers} />
-        <div>
-            <h1 className="main-headingtitle">A <span className="ChangeHeadText"> NEKED </span> SZÓLÓ <br /> ÉLMÉNY</h1>
-            <h2 className="main-headingtitle2"> PODCAST<span className="ChangeHeadText">/STÚDIÓBÉRLÉS</span></h2>
-            <h2 className="main-headingtitle2">TEL: 06 30 337 9962</h2>
-        </div>
-        </div>
-           <Outlet />
+            <div className="Header-img">
+                <Navbar leftSide={navbarLeftSide} rightSide={navbarRightSide} rightSideOthers={navbarRightSideOthers} />
+                <div>
+                    <h1 className="main-headingtitle">A <span className="ChangeHeadText"> NEKED </span> SZÓLÓ <br /> ÉLMÉNY</h1>
+                    <h2 className="main-headingtitle2"> PODCAST<span className="ChangeHeadText">/STÚDIÓBÉRLÉS</span></h2>
+                    <h2 className="main-headingtitle2">TEL: 06 30 337 9962</h2>
+                </div>
+            </div>
+            <Outlet />
         </main>
-     );
+    );
 }
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,37 +6,37 @@ function Navbar(props) {
     const { leftSide, rightSide, rightSideOthers } = props;
     return (
         <div className="container-fluid navbar-container">
-
-
             <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
-                <div className="container-fluid">
+                <div className="container-fluid"style={{ padding: "10px"}}>
                     <button
-                        className="navbar-toggler"
+                        className="navbar-toggler"style={{ padding: "10px"}}
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent"
                         aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        aria-label="Toggle navigation"
+                        >
+                        <span className="navbar-toggler-icon "></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                        <ul className="navbar-nav me-auto mb2 mb-lg-0">
-                            {leftSide.map((navItem) => (<li key={navItem.to} className="nav-item">
-                                <Link className="nav-link" to={navItem.to}>
-                                    {navItem.text}
-                                </Link>
-                            </li>))}
-                        </ul>
-                        <ul className="navbar-nav me-auto mb2 mb-lg-0">
-                            {leftSide.map((navItem) => (<li key={navItem.to} className="nav-item">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ padding: "10px"}} >
+                    <ul className="navbar-nav me-auto mb2 mb-lg-0">
+                            {leftSide.map((navItem) => (<li key={navItem.key} className="nav-item">
                                 <Link className="nav-link" to={navItem.to}>
                                     {navItem.ImgLogo}
                                 </Link>
                             </li>
                             ))}
                         </ul>
+                        
+                        <ul className="navbar-nav me-auto mb2 mb-lg-0">
+                            {leftSide.map((navItem) => (<li key={navItem.key} className="nav-item">
+                                <Link className="nav-link" to={navItem.to}>
+                                    {navItem.text}
+                                </Link>
+                            </li>))}
+                        </ul>
+                       
                         <ul className="navbar-nav">
                             {rightSide.map((navItem) => (<li key={navItem.to} className="nav-item">
                                 <Link className="nav-link" to={navItem.to}>
