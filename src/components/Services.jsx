0c48102381eb7467e36cb0c4df006501349cb691
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-25 10:18:45 +0200

    homepage-finish

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,48 +8,81 @@ function Services() {
                 <div className="card-body text-center">
                     <h2 className="card-title">Basic Csomag</h2>
                     <br />
-                    <p className="card-text">Alapfelszerelés: mikrofonok, fejhallgatók stb.
-                        Segítség a felvétel indításához és leállításához
-                        Alapvető hangmérnöki támogatás
-                        Vágatlan anyag átadása adathordozón és felhőszolgáltatáson
+                    <p className="card-text">
+                        <h4>A csomag tartalma</h4>
+                        <ul className="list-unstyled mt-3 mb mb-4">
+                            <li>mikrofonok</li>
+                            <li>2 db tablet</li>
+                            <li>2 db go pro kamera</li>
+                            <li>fejhallgatók</li>
+                            <li>Segítség a felvétel indításához és leállításához</li>
+                            <li>Alapvető hangmérnöki támogatás</li>
+                            <li>Utómunka</li>
+                            <li>Vágatlan anyag átadása hozott adathordozón</li>
+                            <li style={{fontStyle:'italic', color: '#297CA4'}}>Egyedi elképzelését, ha van, kérjük jelezze felénk!</li>
+                        </ul>
                     </p>
                     <div className="card-footer">
-                        <h3>Ár: 18 000 Ft/óra</h3>
-                        <a href="#" className="btn btn-primary">Tovább a foglaláshoz</a>
+                        <h3>Ár: 50 000 Ft</h3>
+                        <p>Stúdióbérlés: 8000 Ft/óra</p>
+                        <a href="/login"className="btn btn-primary">Tovább a foglaláshoz</a>
                     </div>
                 </div>
             </div>
             <div className="card" data-bs-theme="dark">
-                <img className="card-img-top" src="/images/szolgaltatasok1.jpg" alt="Card image cap" />
+                <img className="card-img-top" src="/images/szolgaltatasok3.jpg" alt="Card image cap" />
                 <div className="card-body text-center">
                     <h2 className="card-title">Basic Csomag Plusz</h2>
                     <br />
-                    <p className="card-text">Alapfelszerelés: mikrofonok, fejhallgatók stb.
-                        Segítség a felvétel indításához és leállításához
-                        Alapvető hangmérnöki támogatás
+                    <p className="card-text">
+                        <h4>A csomag tartalma</h4>
+                        <ul className="list-unstyled mt-3 mb mb-4">
+                            <li>mikrofonok</li>
+                            <li>2 db go pro kamera</li>
+                            <li>2 db tablet</li>
+                            <li>fejhallgatók</li>
+                            <li>Segítség a felvétel indításához és leállításához</li>
+                            <li>Hangmérnök bérlése</li>
+                            <li>Felvétel professzionális kamarákkal (2db)</li>
+                            <li> Vágás,utómunka</li>
+                            <li>Podcast műsor arculat tervezés</li>
+                            <li>Vágatlan anyag átadása adathordozón és felhőszolgáltatáson keresztül</li>
+                            <li style={{fontStyle:'italic', color: '#297CA4'}}>Egyedi elképzelését, ha van, kérjük jelezze felénk!</li>
+                        </ul>
                     </p>
                     <div className="card-footer">
-                        <h3>Ár: 12 000 Ft/óra</h3>
-                        <a href="#" className="btn btn-primary">Tovább a foglaláshoz</a>
+                        <h3>Ár: 180 000 Ft/alkalom</h3>
+                        <p>Stúdióbérlés: 8000 Ft/óra</p>
+                        <a href="/login" className="btn btn-primary">Tovább a foglaláshoz</a>
                     </div>
                 </div>
             </div>
             <div className="card" data-bs-theme="dark">
-                <img className="card-img-top img-fluid" src="/images/szolgaltatasok1.jpg" alt="Card image cap" />
+                <img className="card-img-top img-fluid" src="/images/szolgaltatasok4.jpg" alt="Card image cap" />
                 <div className="card-body text-center">
                     <h2 className="card-title">Business Csomag</h2>
                     <br />
-                    <p className="card-text">Egyéni hangmérnök és producerek rendelkezésre állása
-                        Testreszabott podcast tervezés és tartalomfejlesztés
-                        Kiterjesztett utómunka szolgáltatások: zenei betétek, hanghatások, grafikai dizájn stb.
-                        Felhőalapú anyag tárolás és letöltés
-                        Vágás és utómunka
-                        Social media megjelenési opciók
-                        Arculattervezés
+                    <p className="card-text">
+                        <h4>A csomag tartalma</h4>
+                        <ul className="list-unstyled mt-3 mb mb-4">
+                            <li>mikrofonok</li>
+                            <li>2 db go pro kamera</li>
+                            <li>2 db tablet</li>
+                            <li>fejhallgatók</li>
+                            <li>Segítség a felvétel indításához és leállításához</li>
+                            <li>Hangmérnök bérlése</li>
+                            <li>Felvétel professzionális kamarákkal (2db)</li>
+                            <li>Podcast műsor arculat tervezés</li>
+                            <li>Live streaming, YouTube, Facebook felületekre</li>
+                            <li>Élőben vágás</li>
+                            <li>Vágatlan anyag átadása adathordozón és felhőszolgáltatáson keresztül</li>
+                            <li style={{fontStyle:'italic', color: '#297CA4'}}>Egyedi elképzelését, ha van, kérjük jelezze felénk!</li>
+                        </ul>
                     </p>
                     <div className="card-footer">
-                        <h3>Ár: 12 000 Ft/óra</h3>
-                        <a href="#" className="btn btn-primary">Tovább a foglaláshoz</a>
+                        <h3>Ár: 180 000 Ft/alkalom</h3>
+                        <p>Stúdióbérlés: 8000 Ft/óra</p>
+                        <a href="/login"className="btn btn-primary">Tovább a foglaláshoz</a>
                     </div>
                 </div>
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,8 +14,8 @@ function HomePage() {
         <div className="container">
             <div className="row align-items-center">
                 <div className="col-md-6 text-end">
-                    <h3>Engedd, hogy bemutassam a Podcastudio csodálatos világát! Vadonatúj épületünkben lenyűgöző belső terek és letisztult design vár, melyeket a legkiválóbb elektronikai eszközök tesznek tökéletessé. Hangrögzítésre, élő közvetítésre és még a saját eszközeid bevonására is lehetőség van. Itt minden adott a magas minőségű podcastek létrehozásához és megosztásához!
-                        Kíváncsi vagy további részletekre? Kattints most, hogy felfedezd, hogyan lehet saját podcastjeidnek otthont adni ebben a lenyűgöző stúdióban! Bérlési lehetőségeinkről és további részletekről itt olvashatsz.</h3>
+                    <p>Engedd, hogy bemutassam a Podcastudio csodálatos világát! Vadonatúj épületünkben lenyűgöző belső terek és letisztult design vár, melyeket a legkiválóbb elektronikai eszközök tesznek tökéletessé. Hangrögzítésre, élő közvetítésre és még a saját eszközeid bevonására is lehetőség van. Itt minden adott a magas minőségű podcastek létrehozásához és megosztásához!
+                        Kíváncsi vagy további részletekre? Kattints most, hogy felfedezd, hogyan lehet saját podcastjeidnek otthont adni ebben a lenyűgöző stúdióban! Bérlési lehetőségeinkről és további részletekről itt olvashatsz.</p>
                 </div>
                 <div className="col-md-6 text-start"
                     style={{ backgroundImage: `url("/images/szolgaltatasok2.jpg")`, backgroundSize: "cover", backgroundPosition: "center", minHeight: "50vh" }}>
@@ -23,9 +23,54 @@ function HomePage() {
                 </div>
             </div>
         </div>
+        <h1 className="main-headingtitle3"> FOGLALÁSI<span className="ChangeHeadText">/CSOMAGOK</span></h1>
         <div className="container">
-            <Services />
+            <div className="align-items-center">
+                <div className="container">
+                    <h3>A stúdió leírása</h3>
+                    <p>
+
+                        A podcaststúdiónk egy ízlésesen berendezett jóhangulaú, helyszín, amely tökeletes áthangolódást biztosít mind az előadó, mind a beszélgetőpartner számára. <br />
+                        A stúdió helyszínhez egy alapszolgáltatási csomag kapcsolódik, amely kiegészíthető választható extra szolgáltatásokkal.
+
+                        A stúdió bérlése
+                        8000 Ft+Áfa/óra
+
+                        HANG- és képi RÖGZÍTÉS, két mikrofonnal, 2 db kamerával rögzítjük. A kész hang- és képanyagot vágatlanul
+                        átatdjuk a megrendelőnek, az általa hozott adathordozón.
+
+
+                        A szolgáltatást 2 db bp40-es podcast mikrofon valamint egy RODE Rodecaster PRO podcast keverőpult biztosítja.
+                    </p>
+                    <div className="col-sm-12"
+                        style={{ backgroundImage: `url("/images/studiorol.jpg")`, backgroundPosition: "center", minHeight: "60vh",  backgroundRepeat: "no-repeat", }}>
+
+                    </div>
+                </div>
+
+            </div>
+            <div className="container">
+                <Services />
+            </div>
+            <div className="col-lg-6 mx-auto" style={{ fontStyle: "italic", fontWeight: "bold" }}>
+            <div>
+                <p>A foglalás csak regisztrált/bejelentkezett felhasználók számára elérhető!
+                    Kérjük, ha még nem regisztráltál akkor kattints a regisztrációs oldalra.
+
+                    Ha már regisztráltál kérjük hitelesítsd a regisztrációd az postafiókban
+                    megjelenő visszaigazoló linkre kattintva.
+
+                    Jelentkezz be hozzánk és máris indíthatod a foglalást!</p>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <a href="/register" className="btn btn-primary">Regisztráció</a>
+                    <a href="/login" className="btn btn-primary">Bejelentkezés</a>
+                </div>
+
+            </div>
         </div>
+        </div>
+
+        
 
     </div>);
 }
